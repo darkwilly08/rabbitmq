ae9c8cd856b12c7515de45884e643601929c6657
darkwilly08 <darkwilly08@hotmail.com>
2022-10-13 20:53:19 -0300

    feat: development and production environment ready

--- clients/node/app.js
+++ clients/node/app.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 const amqp = require("amqplib");
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
-const run = async () => {
-  const conn = await amqp.connect("amqps://darkwilly08:123asd456@ubuntu//", {
+const amqpsUrl = process.env.AMQPS_URL;
+let amqpOptions = null;
+if (process.env.NODE_ENV !== "production") {
+  amqpOptions = {
     ca: [
       fs.readFileSync(
         "C:/Users/darkw/Documents/NetBeansProjects/rabbitmq/certificates/ca_certificate.pem"
       ),
     ],
-  });
+  };
+}
+
+const run = async () => {
+  const conn = await amqp.connect(amqpsUrl, amqpOptions);
 
   console.log("connection", conn);
 };
